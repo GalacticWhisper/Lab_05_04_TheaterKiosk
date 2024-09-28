a8c6c38f77f8b0ef056ca7a126705f0be2d11d0a
IanM <ianmarr@Ians-MacBook-Air.local>
2024-09-28 16:54:44 -0400

    InitialCommit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int userAge;
+            System.out.print("Welcome to the theater. Please enter your age: ");
+            if(in.hasNextInt())
+            {
+                userAge = in.nextInt();
+                if(userAge >= 21)
+                {
+                    System.out.print("Your age is sufficient. I bestow upon thee a wristband crafted from the finest of recycled paper.");
+
+                }
+            }
     }
 }
